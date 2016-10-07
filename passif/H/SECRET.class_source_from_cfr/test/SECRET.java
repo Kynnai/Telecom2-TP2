b1902528e646a1f3ccfaaa7a2661184049006ac9
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-10-07 14:22:03 -0400

    ajout d'un flag

---
+++ passif/H/SECRET.class_source_from_cfr/test/SECRET.java
@@ -0,0 +1,15 @@
+/*
+ * Decompiled with CFR 0_115.
+ */
+package test;
+
+public class SECRET {
+    private static final String SECRET_PREFIX = "FLAG-";
+
+    public String flagIsReturnedHere() {
+        int v1 = 7;
+        v1 = v1 * 10 + 7;
+        v1 = v1 * 10 + 5;
+        return "FLAG-" + v1 + "839667";
+    }
+}
